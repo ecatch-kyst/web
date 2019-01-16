ff090f5b5e0ca48d6309f822098b668ba46975f7
Balázs Orbán <info@balazsorban.com>
2019-01-16 09:19:13 +0100

    🎉 initial commit

---
+++ src/setupTests.js
@@ -0,0 +1,8 @@
+import React from "react"
+import {configure, mount, shallow} from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+configure({adapter: new Adapter()})
+
+global.React = React
+global.mount = mount
+global.shallow = shallow
