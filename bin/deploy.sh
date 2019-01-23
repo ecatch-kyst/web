f516f81334aa63cad4793615b4e9b1ef18ef6be2
Balázs Orbán <info@balazsorban.com>
2019-01-23 11:10:34 +0100

    👌 run Lighthouse with respect to the PR target

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-BRANCH_REGEX="^master|develop$"
-
-if [ $TRAVIS_BRANCH =~ $BRANCH_REGEX ]; then
-  firebase deploy -P $TRAVIS_BRANCH --token $FIREBASE_TOKEN
-elif [ $TRAVIS_PULL_REQUEST != false ]; then
-  firebase deploy -P develop --token $FIREBASE_TOKEN
+if [[ "$TRAVIS_BRANCH" =~ ^master|develop$ ]]
+  then
+  firebase deploy -P "$TRAVIS_BRANCH" --token "$FIREBASE_TOKEN"
+elif [ "$TRAVIS_PULL_REQUEST" != false ]
+  then
+  firebase deploy -P develop --token "$FIREBASE_TOKEN"
 else
   echo "No need to deploy."
 fi
