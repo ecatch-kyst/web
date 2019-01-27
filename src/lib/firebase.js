31d0fd571ae3f4fd95e7f09c3e6594e484b62ec9
Balázs Orbán <info@balazsorban.com>
2019-01-27 16:47:06 +0100

    🔧 add Firebase 🔥 config

---
+++ src/lib/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "@firebase/app"
+import "@firebase/database"
+import "@firebase/firestore"
+import "@firebase/auth"
+import "@firebase/storage"
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD5l09unKGP3D3cFY_mr_BQPgcTjhkU2jc",
+  authDomain: "ecatch-kyst.firebaseapp.com",
+  databaseURL: "https://ecatch-kyst.firebaseio.com",
+  projectId: "ecatch-kyst",
+  storageBucket: "ecatch-kyst.appspot.com",
+  messagingSenderId: "1037945604073"
+})
+
+export const FS = firebase.firestore()
+FS.settings({timestampsInSnapshots: true})
+
+export const DB = firebase.database()
+export const FileStore = firebase.storage()
+export const AUTH = firebase.auth()
+
+export const TIMESTAMP = firebase.firestore.FieldValue.serverTimestamp()
+
+export default firebase
