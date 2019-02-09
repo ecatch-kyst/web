1f13d3ba96712a308dac2c3f629dd95977825fac
Balázs Orbán <info@balazsorban.com>
2019-02-09 18:51:57 +0100

    ✅ update tests

--- src/components/__tests__/Dashboard.test.js
+++ src/components/__tests__/Dashboard.test.js
@@ -1,10 +1,14 @@
 import Dashboard from "../Dashboard"
 
+import "../../lib/firebase"
+
+jest.mock("../../lib/firebase", () => ({
+  AUTH: {currentUser: {}}
+}))
+
+
 describe("Dashboard component", () => {
-  const props = {
-    t: text => text,
-    store: {user: {displayName: "name"}}
-  }
-  const wrapper = mount(<Dashboard {...props}/>)
+  const props = {t: text => text}
+  const wrapper = shallow(<Dashboard {...props}/>).dive()
   it("renders correctly", () => expect(wrapper).toHaveLength(1))
 })
