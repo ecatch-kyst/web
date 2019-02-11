19c64d5cd5ba2f178e928a1f52262bd90764f7ab
Balázs Orbán <info@balazsorban.com>
2019-02-11 23:12:56 +0100

    🤡 update mocks

--- src/__mocks__/react-router-dom.js
+++ src/__mocks__/react-router-dom.js
@@ -6,5 +6,6 @@ module.exports = {
   Route: ({children}) => <div>{children}</div>,
   Link: ({children, to}) => <a href="mocked" to={to}>{children}</a>,
   withRouter: () => Component => props => <BrowserRouter><Component {...props}/></BrowserRouter>,
-  Redirect: ({to}) => <div to={to}/>
+  Redirect: ({to}) => <div to={to}/>,
+  Switch: ({children}) => <div>{children}</div>
 }
