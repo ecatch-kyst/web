14ec327aaad366efd8523c077dec8cce596d1547
Balázs Orbán <info@balazsorban.com>
2019-02-11 23:22:53 +0100

    ✨ add DestructButton component

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -1,8 +1,11 @@
 import Loading from "./Loading"
+import DestructButton from "./DestructButton"
 import Dialog from "./Dialog"
 import Page, {withPage} from "./Page"
 
+
 export {
+  DestructButton,
   Loading,
   Page,
   Dialog,
