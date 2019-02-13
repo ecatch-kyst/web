6004502c5dbf400d9f5159da66d194c6f72941f1
Balázs Orbán <info@balazsorban.com>
2019-02-13 11:42:05 +0100

    🔥 remove unused import

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {withTranslation} from 'react-i18next'
 import {withPage} from './shared'
-import InputDropdown from './shared/InputDropdown'
 import Departure from './Departure'
 
 
