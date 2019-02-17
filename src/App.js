6bd5b39588fde6a4b4ab22b60d99753ef1ea651c
Balázs Orbán <info@balazsorban.com>
2019-02-17 12:35:00 +0100

    👌 update code due to testing

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import {
   NotFound,
   Dialog
 } from './components'
-import {withTranslation} from 'react-i18next'
+import {withTranslation, useTranslation} from 'react-i18next'
 
 
 export const App = ({theme: {palette: {type}}}) =>
@@ -30,7 +30,7 @@ export const App = ({theme: {palette: {type}}}) =>
       <Route component={NotFound}/>
     </Switch>
     <Route
-      render={({location: {pathname}}) => ![routes.ROOT, routes.REGISTER].includes(pathname) ? <Navigation/> : null}
+      render={({location: {pathname}}) => ![routes.ROOT, routes.REGISTER].includes(pathname) ? <Navigation value={pathname.slice(1)} /> : null}
     />
     <OfflineStatus/>
     <Dialog/>
@@ -53,11 +53,12 @@ const navigation = [
   }
 ]
 
-export const Navigation = withTranslation("common")(withRouter(
-  ({t, location: {pathname}}) =>
+export const Navigation = ({value}) => {
+  const [t] = useTranslation("common")
+  return (
     <BottomNavigation
       style={{position: "fixed", bottom: 0, width: "100vw"}}
-      value={pathname.replace("/", "")}
+      value={value}
     >
       {navigation.map(({id, icon, to}) =>
         <BottomNavigationAction
@@ -70,4 +71,5 @@ export const Navigation = withTranslation("common")(withRouter(
         />
       )}
     </BottomNavigation>
-))
+  )
+}
