2e20b43813030f3e46f36dac3f15d96a348505ff
Balázs Orbán <info@balazsorban.com>
2019-02-17 16:36:42 +0100

    🎨 improve code
    Centered and Page components are more customizable now.
    
    Centered's style can now receive props, while Page can take a title prop, that overrides the built-in title that comes from the namespace. WARNING! ⚠ This title is not translated 🌐, use with caution, where title is international or no language specific.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {withTranslation} from 'react-i18next'
 import {withPage} from './shared'
 import Departure from './Departure'
 
 
-const Dashboard = () => <div>
-  <Departure/>
-</div>
+const Dashboard = () =>
+  <>
+    <Departure/>
+  </>
 
-export default withTranslation("pages")(withPage(Dashboard, {namespace: "dashboard"}))
+export default withPage(Dashboard, {namespace: "dashboard"})
