b7378dceb81b78b1c1e539f44d58886d2bab9f0d
Balázs Orbán <info@balazsorban.com>
2019-02-17 16:58:16 +0100

    🔒 form validation placeholder
    #32, #13, #29

---
+++ src/utils/validate.js
@@ -0,0 +1,8 @@
+export default validate = {
+  ZD: v => typeof v === "string",
+  ZT: v => typeof v === "string",
+  PD: v => typeof v === "string",
+  ZT: v => typeof v === "string",
+  LA: v => typeof v === "number",
+  LO: v => typeof v === "number"
+}
