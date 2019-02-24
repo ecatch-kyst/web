70eeb248048b2ec3303020e75938e3c5704db61b
Balázs Orbán <info@balazsorban.com>
2019-02-24 22:55:15 +0100

    💄 improve Notification UI
    change notification background, instead of action button color

---
+++ src/components/shared/Notification.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import {useTranslation} from 'react-i18next'
+import {Snackbar, SnackbarContent, Button, withTheme} from '@material-ui/core'
+import {colors} from '../../lib/material-ui'
+import {useNotification} from '../../hooks'
+
+export const Notification = ({theme}) => {
+
+  const {
+    open, name, type, handleAction, duration, message,
+    processQueue,
+    close
+  } = useNotification()
+
+  const handleClose = (_, reason) => {
+    if (reason === 'clickaway') return
+    close()
+  }
+
+  const [t] = useTranslation("common")
+
+  let actionColor
+  let backgroundColor = theme.palette.primary.main
+  switch (type) {
+  case "success":
+    backgroundColor = colors.green
+    break
+  case "error":
+    backgroundColor = colors.red
+    break
+  case "warning":
+    backgroundColor = colors.yellow
+    break
+  case "info":
+    backgroundColor = colors.blue
+    break
+  default:
+    actionColor = "#fff"
+    break
+  }
+
+  let ActionButton = null
+  if (handleAction) {
+    ActionButton =
+      <Action
+        color={actionColor}
+        onClick={handleAction}
+        title={t(`notifications.${name}.action`)}
+        type={type}
+      />
+  }
+
+
+  return (
+    <Snackbar
+      anchorOrigin={{vertical: "bottom", horizontal: "right"}}
+      autoHideDuration={duration}
+      key={name+type}
+      onClose={handleClose}
+      onExited={processQueue}
+      open={open}
+    >
+      <SnackbarContent
+        action={ActionButton}
+        message={t(`notifications.${name}.${type || "default"}`, {message})}
+        style={{backgroundColor}}
+      />
+    </Snackbar>
+  )
+}
+
+export default withTheme()(Notification)
+
+export const Action =({title, onClick}) =>
+  <Button onClick={onClick}>{title}</Button>
