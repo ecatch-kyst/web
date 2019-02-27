535cec59ea7d7cf54416340be9e73cf382598fad
Morten <mortefal@stud.ntnu.no>
2019-02-27 18:37:48 +0100

    :construction: add function in actions, to send and retrieve from FB
    WIP

--- src/db/actions/customlists.js
+++ src/db/actions/customlists.js
@@ -1,3 +1,7 @@
+import {AUTH, USERS_FS} from "../../lib/firebase"
+import {flattenDoc} from "../../utils"
+
+
 /**
  * Submit a users own fishingspot to store //TODO: send to firebase
  * @param {int} value
@@ -19,6 +23,12 @@ export function addSpot(){
   this.notify({name: "addSpot", type: "success"})
 }
 
+/**
+ *
+ * @param {string} name
+ * @param {object} value
+ * sets the value from input in the addfishingspot dialog in store.editing
+ */
 export function handle({target: {name, value}}) {
   this.setState(({custom}) => ({
     custom: {
@@ -29,4 +39,35 @@ export function handle({target: {name, value}}) {
       }
     }
   }))
+}
+
+/**
+ * Gets the custom list from Firebase for the logged in user.
+ */
+export function retrieve(){
+  USERS_FS.doc(AUTH.currentUser.uid).collection("fishingspots")
+    .onSnapshot(snap => {
+      this.setState({fishingspots: snap.docs.map(flattenDoc)})
+    }, error => console.error(error)
+    )
+}
+
+
+/**
+ * Function for sending fishingspot to firebase WIP
+ */
+export async function sending(type){
+  try{
+    const{fishingspots} = this.state.custom.fishingspots
+
+    await USERS_FS.doc(AUTH.currentUser.uid).collection("fishingspots").add({
+      ...fishingspots,
+      fishingspots
+    })
+    this.notify({name: `message.sent.${type}`, type: "success"})
+  } catch ({code, message}){
+    this.notify({name: `message.sent.${type}`, type: "error", message: [code, message].join(": ")})
+  }
+
+
 }
