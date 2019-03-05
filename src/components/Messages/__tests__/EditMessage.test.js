2b231ada94ce43db0f47ed4b1215b686bdc59bb0
Ebba Louise Toreld Fingarsen <elfingar@stud.ntnu.no>
2019-03-05 17:33:45 +0100

    :construction: :white_check_mark: change test for editMessage

--- src/components/Messages/__tests__/EditMessage.test.js
+++ src/components/Messages/__tests__/EditMessage.test.js
@@ -1,14 +1,21 @@
 import EditMessage from '../EditMessage'
+import {Redirect} from "react-router-dom"
+import "../../../db"
+import {routes} from "../../../lib/router"
 //{store: {messages}, match: {params: {messageId}}}
+
+jest.mock("../../../db", () => ({
+  withStore: Component => props => <Component {...props}/>
+}))
 
 describe("EditMessage component", () => {
   const props = {
     store: {messages:{
-      RN: 1,
-      TM: "DEP",
-      created: new Date(2014, 6, 10, 23, 0)
+      RN: jest.fn(),
+      TM: "DCA",
+      created: jest.fn()
     }},
-    match: {params: {messageId:{}}}
+    match: {params: {messageId: jest.fn()}}
   }
   const wrapper = shallow(<EditMessage {... props}/>)
 
@@ -16,9 +23,18 @@ describe("EditMessage component", () => {
     expect(wrapper).toHaveLength(1)
   })
 
-  it("created", () => {
-    wrapper.setProps({messages:{created: new Date(2014, 6, 10, 23, 0)}})
-    expect(wrapper.prop("created")).toBe(new Date(2014, 6, 10, 23, 0))
+  /*
+  it('correct messageId', () => {
+    wrapper.setProps({store:{messages:{RN: 1}}})
+    wrapper.setProps({match:{params:{messageId:1}}})
+    expect({store:{messages:{RN}}}).toBe({match:{params:{messageId}}})
+  })*/
+
+  it("if created is too old, redirect to MESSAGES", () => {
+    wrapper.setProps({store:{messages:{created: new Date(2018, 6, 10, 23, 0)}}})
+    expect(wrapper.find(Redirect)).toHaveLength(1) //this fails for now, the length is 0.
+    expect(wrapper.find(Redirect).prop("to")).toBe(routes.MESSAGES)
+    wrapper.setProps({store:{messages:{created: Date.now()}}})
   })
 
 })
