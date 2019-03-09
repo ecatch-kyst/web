9e4a897576085e721d7076d891125262145521b0
Petter Rein <prein31@gmail.com>
2019-03-09 15:47:36 +0100

    Revert "add persistence to Firestore #39"
    This reverts commit f0c0db4ae177cd025d6516de576f8aaa8a114d9e.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,34 +4,16 @@ import "@firebase/firestore"
 import "@firebase/auth"
 import "@firebase/storage"
 
-
-const config = {
+firebase.initializeApp({
   apiKey: "AIzaSyD5l09unKGP3D3cFY_mr_BQPgcTjhkU2jc",
   authDomain: "ecatch-kyst.firebaseapp.com",
   databaseURL: "https://ecatch-kyst.firebaseio.com",
   projectId: "ecatch-kyst",
   storageBucket: "ecatch-kyst.appspot.com",
   messagingSenderId: "1037945604073"
-}
-
-export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app()
+})
 
 export const FS = firebase.firestore()
-FS.enablePersistence()
-  .catch(({code}) => {
-    switch (code) {
-    case 'failed-precondition':
-      // Multiple tabs open, persistence can only be enabled
-      // in one tab at a time.
-      break
-    case 'unimplemented':
-      // The current browser does not support all of the
-      // features required to enable persistence
-      break
-    default:
-      break
-    }
-  })
 export const DB = firebase.database()
 export const FileStore = firebase.storage()
 export const AUTH = firebase.auth()
@@ -42,3 +24,5 @@ export const GEOPOINT = (lat, long) => new firebase.firestore.GeoPoint(lat, long
 
 export const CONNECTION_REF = DB.ref(".info/connected")
 export const USERS_FS = FS.collection("users")
+
+export default firebase
