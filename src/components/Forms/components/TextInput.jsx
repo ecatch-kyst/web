ac7379955fd4df7948276755692e59f216f591e3
Balázs Orbán <info@balazsorban.com>
2019-03-18 13:09:40 +0100

    ⚡️ simplify TextInput

--- src/components/Forms/components/TextInput.jsx
+++ src/components/Forms/components/TextInput.jsx
@@ -1,27 +1,24 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {TextField, InputAdornment} from "@material-ui/core"
 
 export default ({dataId, label, onChange, type, value, unit, disabled}) => {
 
-  const [localValue, setValue] = useState("")
-
-  // update local state, when global is updated
-  useEffect(() => {setValue(value || "")}, [value])
+  const [localValue, setValue] = useState(value || "")
 
   // when user inputs something into the text field, update the local state
   const handleChange = ({target: {value}}) => {
-    setValue(type === "number" ? parseInt(value, 10) : (value || ""))
+    setValue((type === "number" ? parseInt(value, 10) : value) || "")
   }
 
   // when user moves away from the field, update the global state
-  const handleBlur = () => onChange(dataId, localValue)
+  const handleBlur = () => onChange(dataId, localValue, type)
 
   return (
     <TextField
-      disabled={disabled}
       InputProps={{
         startAdornment: unit ? <InputAdornment position="start">{unit}</InputAdornment> : null
       }}
+      disabled={disabled}
       label={label}
       onBlur={handleBlur}
       onChange={handleChange}
