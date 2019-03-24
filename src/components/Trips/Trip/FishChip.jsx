54b0932c3cb140e4bf5a907d6da49a035fee0256
Balázs Orbán <info@balazsorban.com>
2019-03-24 11:44:45 +0100

    🔥 remove unused import
    #88

--- src/components/Trips/Trip/FishChip.jsx
+++ src/components/Trips/Trip/FishChip.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {withTranslation, useTranslation} from 'react-i18next'
+import {useTranslation} from 'react-i18next'
 import {Tooltip, Chip} from '@material-ui/core'
 
 export default ({type, weight}) => {
