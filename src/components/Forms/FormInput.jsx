531b2af1a12d5e76ebafe30323b7a0af9e68ad9a
Unknown <prein31@gmail.com>
2019-04-03 00:23:40 +0200

    :construction: validation on all fields

--- src/components/Forms/FormInput.jsx
+++ src/components/Forms/FormInput.jsx
@@ -7,6 +7,7 @@ import DropdownMap from './components/DropdownMap'
 import GeoPointInput from './components/GeoPointInput'
 import {useStore} from '../../hooks'
 
+
 /**
  * Dynamic input field
  * @param {object} props
@@ -17,13 +18,78 @@ import {useStore} from '../../hooks'
  */
 
 export const FormInput = ({id, dataId, type, options}) => {
+  const [tList] = useTranslation("dropdowns")
+
+  const fishTool = tList("fishing-gear", {returnObjects: true})
+
+  const fish = tList("species", {returnObjects: true})
+
+  const activites = tList("activity", {returnObjects: true})
 
   const {handleFieldChange, fields} = useStore()
   const [t] = useTranslation("forms")
 
+  /** IsString */
+  function isString(s) {
+    return (typeof s === 'string' || s instanceof String)
+  }
+
+  /** Validate fields */
+  const validate = (name, value) => {
+    switch (name){
+    case "PO":
+      return (isString(value) && value.length === 5)
+    // Look up case fall-through for expalining the 3 case down bellow
+    // TODO: Add check for expectedFishingStart is after depature osv
+    case "expectedFishingStart":
+    case "fishingStart":
+    case "departure":
+    case "portArrival":
+      return (new Date(value)).getTime() > 0
+    case "AC":
+      return (activites.map(({value}) => value)).includes(value)
+    case "endFishingSpot":
+    case "startFishingSpot":
+    case "expectedFishingSpot":
+      // TODO: Find something better
+      var patt = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/
+      return !!(`${value.latitude },${ value.longitude}`).match(patt)
+    case "DS":
+      return (fish.map(({value}) => value)).includes(value)
+    case "CA":
+    case "OB":
+    case "KG":
+      // TODO: Fix NaN error when going from -1 to 0 and unfocus field
+      var result = true
+      for (var object in value){
+        result = ((fish.map(({value}) => value)).includes(object) && (value[object] === "" || value[object] >= 0))
+      }
+      return result
+    case "QI":
+      return (value <= 7 && value >= 1)
+    case "GS":
+      return (value <= 4 && value >= 1)
+    case "DU":
+      return value >= 0
+    case "GE":
+      return (fishTool.map(({value}) => value)).includes(value)
+    case "GP":
+      return (value <= 6 && value >= 0)
+    case "ZO":
+      return (isString(value) && value.length === 3)
+    case "LS":
+      return (isString(value) && value.length > 0)
+    case "ME":
+      return value > 0
+    default:
+      return false
+    }
+  }
+
   const handleChange = (name, value) => {
-    const error = null // TODO: validate[dataId](value) // Validating the field
-    if (error) {
+    const error = validate(name, value) // TODO: validate[dataId](value) // Validating the field
+    console.log(name, value)
+    if (!error) {
       console.error(error) // TODO: Add error notification
     } else {
       handleFieldChange(name, value)
