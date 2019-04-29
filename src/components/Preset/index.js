0d83d152fc9b29cd40c7f858ce33769c6c92e037
Balázs Orbán <info@balazsorban.com>
2019-04-29 18:27:03 +0200

    🔥 remove unused import

--- src/components/Preset/index.js
+++ src/components/Preset/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Page} from '../shared'
 import AddFavorite from "./components/AddFavorite"
-import {Grid, Typography} from '@material-ui/core'
+import {Grid} from '@material-ui/core'
 import {useTranslation} from 'react-i18next'
 
 // type is the type of dropdown, list is the list of choices you have the option to choice among to set the favorites, numberOfChoices is how many favorites per type you allow to user to set
